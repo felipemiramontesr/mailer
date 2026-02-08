b444412164f4415d4bafe2b61618606dd0fdf00b
felipemiramontesr <felipemiramontesr@gmail.com>
2026-02-07 23:41:16 -0600

    fix(security): Define SECURE_PATH to unblock 403 Forbidden in access_hash.php

--- public/mailer.php
+++ public/mailer.php
@@ -5,6 +5,7 @@
  */
 
 // --- Pre-flight Headers & Security ---
+define('SECURE_PATH', true);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
